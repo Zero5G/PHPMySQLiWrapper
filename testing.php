305a831f062e384b7a0a2ed502a32e0a10a3d675
Zero5G <jakmanda05@gmail.com>
2025-03-21 19:36:13 +0100

    create destruct, reorder class and add better error catching

--- testing.php
+++ testing.php
@@ -4,6 +4,6 @@ include("lib.php");
 $test = new Database("localhost", "xmanousek", "Ylrq2117", "xmanousek", 7070);
 // $test->delete("test", ["test"], ["16"]);
 // $test->insert("test", ["stra"], ["a"]);
-$test->select("test", "*");
-echo var_dump($test->get_array_table()->get_values());
+// $test->select("test", "*");
+echo var_dump($test->collumn_names());
 ?>
