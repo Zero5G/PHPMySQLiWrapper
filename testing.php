7093fe84773597c74946de595b9758c301e0068c
Zero5G <jakmanda05@gmail.com>
2025-03-22 16:10:11 +0100

    namespace and a get method

--- testing.php
+++ testing.php
@@ -1,9 +1,15 @@
 <?php
-include("lib.php");
-
+require_once "wrapper.php";
+use MySQLiWrapper\ {
+    Database,
+    Table,
+    Last
+};
+//include("lib.php");
 $test = new Database("localhost", "xmanousek", "Ylrq2117", "xmanousek", 7070);
 // $test->delete("test", ["test"], ["16"]);
 // $test->insert("test", ["stra"], ["a"]);
-// $test->select("test", "*");
-echo var_dump($test->collumn_names());
+$test->select("test", "*");
+echo var_dump($test->get("result"));
+echo $test->get_public_properties();
 ?>
